bcc082dcbf95d8f8d61615c672c675dcc1afd649
Yehuda <s0737422@gmail.com>
2026-02-10 15:57:29 +0200

    I add ips founder

--- checks.py
+++ checks.py
@@ -2,3 +2,6 @@ from config import *
 
 def suspicious_ips(data):
     return [list for list in data if list[1][:3] not in EXTERNAL_IP and list[1][:7] not in EXTERNAL_IP]
+
+def suspicious_ports(data):
+    return [list for list in data if list[4] in SENSITIVE_PORT]
